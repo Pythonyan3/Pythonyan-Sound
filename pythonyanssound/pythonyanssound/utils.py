0b573f3da3c362bf12288b42a60f49f70076ad26
Muffin3 <god.coder3@gmail.com>
2022-01-04 15:00:49 +0300

    :sparkles: update; add docstrings for main Django application;

--- pythonyanssound/pythonyanssound/utils.py
+++ pythonyanssound/pythonyanssound/utils.py
@@ -6,21 +6,34 @@ from rest_framework.views import exception_handler
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 
-def custom_exception_handler(exc, context):
+def custom_exception_handler(exc: Exception, context) -> Response:
+    """Custom exception handler."""
     response = exception_handler(exc, context)
 
     if response is not None:
         return response
     elif isinstance(exc, TokenError):
-        return Response(data={"detail": exc.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            data={"detail": exc.args[0]}, status=status.HTTP_400_BAD_REQUEST
+        )
     elif isinstance(exc, InvalidToken):
-        return Response(data={"detail": exc.args[0]}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            data={"detail": exc.args[0]}, status=status.HTTP_401_UNAUTHORIZED
+        )
     elif isinstance(exc, ObjectDoesNotExist):
-        return Response(data={"detail": "Instance does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            data={"detail": "Instance does not exist"},
+            status=status.HTTP_404_NOT_FOUND
+        )
     elif isinstance(exc, ValidationError):
-        return Response(data={"detail": exc.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            data={"detail": exc.args[0]}, status=status.HTTP_400_BAD_REQUEST
+        )
     elif isinstance(exc, redis.exceptions.ConnectionError):
         return Response(
-            data={"detail": "Connection error has occurred on the server side. Please try it later"},
+            data={
+                "detail": "Connection error has occurred on the server side."
+                          "Please try it later"
+            },
             status=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
--- pythonyanssound/pythonyanssound/validators.py
+++ pythonyanssound/pythonyanssound/validators.py
@@ -3,16 +3,24 @@ import io
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.files.images import get_image_dimensions
+from django.db.models.fields.files import ImageFieldFile, FieldFile
 
 
-def validate_image_resolution(image_field):
+def validate_image_resolution(image_field: ImageFieldFile) -> None:
+    """Validates image resolution defined by settings."""
+    print("Image", type(image_field))
     width, height = get_image_dimensions(image_field.file)
-    if height != settings.APP_IMAGE_HEIGHT or width != settings.APP_IMAGE_WIDTH:
+    if (
+        height != settings.APP_IMAGE_HEIGHT
+        or width != settings.APP_IMAGE_WIDTH
+    ):
         raise ValidationError("Image size should be 1000x1000.")
 
 
-def validate_file_size(file_field):
-    # If file is instance of BytesIO then it is run from Tests (dunno why it works like that)
+def validate_file_size(file_field: FieldFile) -> None:
+    """Validates total file size."""
+    print("File", type(file_field))
+    # creating file from tests passed BytesIO instance
     if isinstance(file_field.file, io.BytesIO):
         pass
     elif file_field.file.size > settings.APP_FILE_MAX_SIZE:
