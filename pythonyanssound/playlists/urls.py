3e8d87250edc574d3be68375fdf8fa682b113ad0
Muffin3 <god.coder3@gmail.com>
2022-01-07 00:49:06 +0300

    :boom: update; add docstrings for 'playlists' Django application;

--- pythonyanssound/playlists/urls.py
+++ pythonyanssound/playlists/urls.py
@@ -1,13 +1,39 @@
 from django.urls import path
 
-from playlists.views import PlaylistListCreateView, PlaylistRetrieveUpdateDeleteView, LikedPlaylistsListView, \
-    ShortPlaylistListView, LikeUnlikePlaylistView, SongAddRemovePlaylistView
+from playlists.views import (
+    PlaylistListCreateView, PlaylistRetrieveUpdateDeleteView,
+    LikedPlaylistsListView, ShortPlaylistListView, LikeUnlikePlaylistView,
+    SongAddRemovePlaylistView
+)
 
 urlpatterns = [
-    path('', PlaylistListCreateView.as_view(), name="own-playlists"),
-    path('short/', ShortPlaylistListView.as_view(), name="short-playlists-list"),
-    path('<int:playlist_id>/', PlaylistRetrieveUpdateDeleteView.as_view(), name="playlist-management"),
-    path('songs/<int:playlist_id>/<int:song_id>/', SongAddRemovePlaylistView.as_view(), name="playlists-songs-management"),
-    path('likes/', LikedPlaylistsListView.as_view(), name="liked-playlists"),
-    path('likes/<int:playlist_id>/', LikeUnlikePlaylistView.as_view(), name="liked-playlists-management"),
+    path(
+        route='',
+        view=PlaylistListCreateView.as_view(),
+        name="own-playlists"),
+    path(
+        route='short/',
+        view=ShortPlaylistListView.as_view(),
+        name="short-playlists-list"
+    ),
+    path(
+        route='<int:playlist_id>/',
+        view=PlaylistRetrieveUpdateDeleteView.as_view(),
+        name="playlist-management"
+    ),
+    path(
+        route='songs/<int:playlist_id>/<int:song_id>/',
+        view=SongAddRemovePlaylistView.as_view(),
+        name="playlists-songs-management"
+    ),
+    path(
+        route='likes/',
+        view=LikedPlaylistsListView.as_view(),
+        name="liked-playlists"
+    ),
+    path(
+        route='likes/<int:playlist_id>/',
+        view=LikeUnlikePlaylistView.as_view(),
+        name="liked-playlists-management"
+    ),
 ]
