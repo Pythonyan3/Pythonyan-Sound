18d52026c75d44ba754d49566c32de82bd5cd8b4
Muffin3 <god.coder3@gmail.com>
2022-01-07 14:08:10 +0300

    :memo: update; add docstrings for 'search' Django application;

--- pythonyanssound/search/apps.py
+++ pythonyanssound/search/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class SearchConfig(AppConfig):
+    """Search Django application config."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'search'
--- pythonyanssound/search/views.py
+++ pythonyanssound/search/views.py
@@ -15,16 +15,11 @@ from pythonyanssound.pagination import CustomPageNumberPagination
 
 
 class SearchListView(APIView):
-    """
-    Processes GET method to obtain search data
-    Allowed only to authenticated users
-    """
+    """Processes GET method to search songs, profiles and playlists."""
     permission_classes = [IsAuthenticated]
 
     def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
+        """Extra context provided to the serializer class."""
         return {
             'request': self.request,
             'format': self.format_kwarg,
@@ -33,29 +28,53 @@ class SearchListView(APIView):
 
     def get(self, request: Request, search_string: str):
         """
-        Returns JSON with corresponds Profiles, Playlists, Songs
+        Returns dict of Profiles, Playlists, Songs lists
+        filtered by 'search_string'.
+
         Performs case insensitive filter by main fields of instances
         (song - title, playlist - title, profile - username)
-        Also divides artist users and non artist users ordered by popularity (followers count)
+        Also divides artist users and non artist users ordered by popularity
+        (followers count)
+        Lists len limited to 10 instances
         """
         # python's slices works like SQL LIMIT
-        artists = Profile.objects.annotate(followers_count=Count("followers")).\
-            filter(username__icontains=search_string, is_artist=True).order_by('-followers_count')[:10]
+        artists = Profile.objects.annotate(
+            followers_count=Count("followers")
+        ).filter(
+            username__icontains=search_string, is_artist=True
+        ).order_by('-followers_count')[:10]
 
-        profiles = Profile.objects.annotate(followers_count=Count("followers")). \
-            filter(username__icontains=search_string, is_artist=False).order_by('-followers_count')[:10]
+        profiles = Profile.objects.annotate(
+            followers_count=Count("followers")
+        ).filter(
+            username__icontains=search_string, is_artist=False
+        ).order_by('-followers_count')[:10]
 
-        playlists = Playlist.objects.filter(title__icontains=search_string)[:10]
-
+        playlists = Playlist.objects.filter(
+            title__icontains=search_string
+        )[:10]
         songs = Song.objects.annotate(
-            is_liked=Exists(request.user.liked_songs.filter(pk=OuterRef("pk"))),
+            is_liked=Exists(
+                request.user.liked_songs.filter(pk=OuterRef("pk"))
+            ),
         ).filter(title__icontains=search_string)[:10]
 
-        artists_serializer = ShortProfileSerializer(instance=artists, many=True, context=self.get_serializer_context())
-        profile_serializer = ShortProfileSerializer(instance=profiles, many=True, context=self.get_serializer_context())
-        playlist_serializer = ListPlaylistsSerializer(instance=playlists, many=True, context=self.get_serializer_context())
-        song_serializer = SongSerializer(instance=songs, many=True, context=self.get_serializer_context())
-
+        artists_serializer = ShortProfileSerializer(
+            instance=artists, many=True, context=self.get_serializer_context()
+        )
+        profile_serializer = ShortProfileSerializer(
+            instance=profiles,
+            many=True,
+            context=self.get_serializer_context()
+        )
+        playlist_serializer = ListPlaylistsSerializer(
+            instance=playlists,
+            many=True,
+            context=self.get_serializer_context()
+        )
+        song_serializer = SongSerializer(
+            instance=songs, many=True, context=self.get_serializer_context()
+        )
         return Response(data={
             "artists": artists_serializer.data,
             "profiles": profile_serializer.data,
@@ -65,60 +84,82 @@ class SearchListView(APIView):
 
 
 class ArtistsSearchView(ListAPIView):
-    """
-    Processes GET method to obtain artists (Profile with is_artist=True) corresponding to search string
-    Allowed only to authenticated users
-    Ordering by artist popularity (followers count)
-    """
+    """Processes GET method to retrieve list of artists."""
     permission_classes = [IsAuthenticated]
     serializer_class = ShortProfileSerializer
     pagination_class = CustomPageNumberPagination
 
     def get_queryset(self):
-        return Profile.objects.annotate(followers_count=Count("followers")).\
-            filter(username__icontains=self.kwargs.get("search_string"), is_artist=True).order_by('-followers_count')
+        """
+        Returns queryset of Profiles with 'is_artist' flag
+        filtered by 'username' with 'search_string'.
+
+        Orders artists by popularity (followers count descending)
+        """
+        return Profile.objects.annotate(
+            followers_count=Count("followers")
+        ).filter(
+            username__icontains=self.kwargs.get("search_string"),
+            is_artist=True
+        ).order_by('-followers_count')
 
 
 class ProfilesSearchView(ListAPIView):
-    """
-    Processes GET method to obtain profiles (Profile with is_artist=False) corresponding to search string
-    Allowed only to authenticated users
-    Ordering by profile followers count
-    """
+    """Processes GET method to retrieve list of artists."""
     permission_classes = [IsAuthenticated]
     serializer_class = ShortProfileSerializer
     pagination_class = CustomPageNumberPagination
 
     def get_queryset(self):
-        return Profile.objects.annotate(followers_count=Count("followers")). \
-            filter(username__icontains=self.kwargs.get("search_string"), is_artist=False).order_by('-followers_count')
+        """
+        Returns queryset of Profiles without 'is_artist' flag
+        filtered by 'username' with 'search_string'.
+
+        Orders artists by popularity (followers count descending)
+        """
+        return Profile.objects.annotate(
+            followers_count=Count("followers")
+        ).filter(
+            username__icontains=self.kwargs.get("search_string"),
+            is_artist=False
+        ).order_by('-followers_count')
 
 
 class PlaylistsSearchView(ListAPIView):
-    """
-    Processes GET method to obtain playlists corresponding to search string
-    Allowed only to authenticated users
-    Ordering by playlist's title
-    """
+    """Processes GET method to retrieve list of playlists."""
     permission_classes = [IsAuthenticated]
     serializer_class = ListPlaylistsSerializer
     pagination_class = CustomPageNumberPagination
 
     def get_queryset(self):
-        return Playlist.objects.filter(title__icontains=self.kwargs.get("search_string")).order_by("title")
+        """
+        Returns queryset of Playlists
+        filtered by 'title' with 'search_string'.
+
+        Orders playlists by 'title' ascending
+        """
+        return Playlist.objects.filter(
+            title__icontains=self.kwargs.get("search_string")
+        ).order_by("title")
 
 
 class SongsSearchView(ListAPIView):
-    """
-    Processes GET method to obtain songs corresponding to search string
-    Allowed only to authenticated users
-    Ordering by song's title
-    """
+    """Processes GET method to retrieve list of songs."""
     permission_classes = [IsAuthenticated]
     serializer_class = SongSerializer
     pagination_class = CustomPageNumberPagination
 
     def get_queryset(self):
+        """
+        Returns queryset of Songs
+        filtered by 'title' with 'search_string'.
+
+        Orders songs by 'title' ascending
+        """
         return Song.objects.annotate(
-            is_liked=Exists(self.request.user.liked_songs.filter(pk=OuterRef("pk"))),
-        ).filter(title__icontains=self.kwargs.get("search_string")).order_by("title")
+            is_liked=Exists(
+                self.request.user.liked_songs.filter(pk=OuterRef("pk"))
+            ),
+        ).filter(
+            title__icontains=self.kwargs.get("search_string")
+        ).order_by("title")
