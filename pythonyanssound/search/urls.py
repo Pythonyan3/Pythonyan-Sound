91a07e03bf295e06b404d1db3142ad5807b28112
Muffin3 <god.coder3@gmail.com>
2022-01-07 14:08:10 +0300

    :memo: update; add docstrings for 'search' Django application;

--- pythonyanssound/search/urls.py
+++ pythonyanssound/search/urls.py
@@ -1,11 +1,35 @@
 from django.urls import path
 
-from search.views import SearchListView, ArtistsSearchView, ProfilesSearchView, PlaylistsSearchView, SongsSearchView
+from search.views import (
+    SearchListView, ArtistsSearchView, ProfilesSearchView,
+    PlaylistsSearchView, SongsSearchView
+)
+
 
 urlpatterns = [
-    path("<str:search_string>/", SearchListView.as_view(), name="search"),
-    path("artist/<str:search_string>/", ArtistsSearchView.as_view(), name="search-artist"),
-    path("profile/<str:search_string>/", ProfilesSearchView.as_view(), name="search-profile"),
-    path("playlist/<str:search_string>/", PlaylistsSearchView.as_view(), name="search-playlist"),
-    path("song/<str:search_string>/", SongsSearchView.as_view(), name="search-song"),
+    path(
+        route="<str:search_string>/",
+        view=SearchListView.as_view(),
+        name="search"
+    ),
+    path(
+        route="artist/<str:search_string>/",
+        view=ArtistsSearchView.as_view(),
+        name="search-artist"
+    ),
+    path(
+        route="profile/<str:search_string>/",
+        view=ProfilesSearchView.as_view(),
+        name="search-profile"
+    ),
+    path(
+        route="playlist/<str:search_string>/",
+        view=PlaylistsSearchView.as_view(),
+        name="search-playlist"
+    ),
+    path(
+        route="song/<str:search_string>/",
+        view=SongsSearchView.as_view(),
+        name="search-song"
+    ),
 ]
